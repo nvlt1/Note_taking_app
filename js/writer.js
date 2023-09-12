5c5f8c6f23fe07f83eb8e8fccfe1298ebb4598cd
Nav <navdeepnx@gmail.com>
2023-09-12 13:16:26 -0700

    fixed writer.js

--- js/writer.js
+++ js/writer.js
@@ -42,47 +42,33 @@ function displayNotes(){
             notes[index].content = event.target.value;
         });
 
+        // create a new button element
         const removeBtn = document.createElement('button');
+        // setting the button text to 'remove'
+        removeBtn.innerText = 'Remove';
+        // adding event listener to the button. Removes current note from the array,
+        // re-display the notes, and save them to local storage
+        removeBtn.addEventListener('click', () => {
+            notes.splice(index, 1);
+            displayNotes();
+            saveNotes();
+        });
+        // appends the textarea and button to the note section,
+        // then note section to the main container
+        noteSection.appendChild(textarea);
+        noteSection.appendChild(removeBtn);
+        container.appendChild(noteSection);
 
 
-    })
-
+    });
 }
+
+// adds event listener to the element with id 'add-button'. When clicked, it'll add
+// a new empty note to the array and re-display the notes
+document.getElementById('add-button').addEventListener('click',
+notes.push({content: ''}));
+
+// Calls displayNotes function initially to show any existing notes when the script runs
+displayNotes();
+
 setInterval(saveNotes, 2000);
-
-
-//         function displayNotes() {
-//             const container = document.getElementById('notes-container');
-//             container.innerHTML = '';
-//             notes.forEach((note, index) => {
-//                 const noteSection = document.createElement('div');
-//                 noteSection.className = 'note-section';
-
-//                 const textarea = document.createElement('textarea');
-//                 textarea.value = note.content;
-//                 textarea.addEventListener('input', (event) => {
-//                     notes[index].content = event.target.value;
-//                 });
-
-//                 const removeBtn = document.createElement('button');
-//                 removeBtn.innerText = 'Remove';
-//                 removeBtn.addEventListener('click', () => {
-//                     notes.splice(index, 1);
-//                     displayNotes();
-//                     saveNotes();
-//                 });
-
-//                 noteSection.appendChild(textarea);
-//                 noteSection.appendChild(removeBtn);
-//                 container.appendChild(noteSection);
-//             });
-//         }
-
-//         document.getElementById('add-button').addEventListener('click', () => {
-//             notes.push({ content: '' });
-//             displayNotes();
-//         });
-
-//         displayNotes();
-
-//         setInterval(saveNotes, 2000);
