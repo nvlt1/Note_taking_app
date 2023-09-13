8274b3ea9ce24c7f3dc67ac98dbadf51d3bfb8d4
Nav <navdeepnx@gmail.com>
2023-09-13 09:53:02 -0700

    completed reader.js and updated html pages

--- js/reader.js
+++ js/reader.js
@@ -1,3 +1,20 @@
-let btn = document.createElement('button');
-btn.innerHTML = 'YES';
-document.body.appendChild(btn);
+// let btn = document.createElement('button');
+// btn.innerHTML = 'YES';
+// document.body.appendChild(btn);
+
+function retrieveNotes(){
+    const notes = JSON.parse(localStorage.getItem('notes')) || [];
+    const container = document.getElementById('notes-display');
+    container.innerHTML = '';
+
+    notes.forEach(note => {
+        const noteSection = document.createElement('div');
+        noteSection.className = 'note-section';
+        noteSection.innerText = note.content;
+        container.appendChild(noteSection);
+    });
+    const currentTime = new Date().toLocaleTimeString();
+    document.getElementById('retrieve-time').innerText = 'Last Retrieved ' + currentTime;
+}
+
+setInterval(retrieveNotes, 2000);
