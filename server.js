2584696b81d08170ad74c29bb9b54a20da101c0f
Nav <navdeepnx@gmail.com>
2023-09-17 15:06:49 -0700

    fixed server and writer.js

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.sendFile("index.html", {root: __dirname})
+});
+
+app.use(express.static(__dirname));
+
 app.listen(3000, () => {
     console.log("Server on 3000");
 })
