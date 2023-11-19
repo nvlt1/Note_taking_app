2c74f3e67f0921c830335e20bb50247db83b1c3f
Nav <navdeepnx@gmail.com>
2023-11-19 14:48:25 -0800

    added input and function for sending sentence

--- COMP4537/project/home.js
+++ COMP4537/project/home.js
@@ -1,7 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
     var logoutButton = document.querySelector('#logoutButton')
+    var sendSentence = document.querySelector('#sendSentenceBtn')
+    var sentenceDisplay = document.querySelector('#sentenceDisplay')
+
     logoutButton.addEventListener('click', function() {
         // document.cookie = ''
-        window.location.href = '/logout.html'
+        window.location.href = '/login.html'
+    });
+
+    sendSentence.addEventListener('click', function() {
+        var sentence = document.querySelector('#sentenceInput').value
+        fetch('api link here', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({sentence: sentence})
+        })
+        .then(response => response.json())
+        .then(data => {
+            sentenceDisplay.textContent = data.response
+        })
+        .catch(error => {
+            console.log("Error: " + error)
+            sentenceDisplay.textContent = "Error occured while sending your sentence SORRY"
+        })
     })
 })
+
+
