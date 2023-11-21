47ad27271ea1a931e473e9066718afe48815ae16
Nav <navdeepnx@gmail.com>
2023-11-21 10:55:44 -0800

    css and admin stuff

--- COMP4537/project/login.js
+++ COMP4537/project/login.js
@@ -11,15 +11,19 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
 
     fetch('https://comp4537-isa-project-back.onrender.com/api/ISA/login', {
         method: 'POST',
+        mode: 'cors',
         headers: {
             'Content-Type': 'application/json'
         },
+        credentials: 'include',
         body: JSON.stringify(reqBody)
     })
     .then(response => response.json())
     .then(data => {
-        if(data.success) {
+        if(data.success && !data.is_admin) {
             window.location.href = '/COMP4537/project/home.html'
+        } else if (data.success && data.is_admin) {
+            window.location.href = '/COMP4537/project/admin.html'
         } else {
             document.getElementById('msgDisplay').textContent = data.message;
         }
